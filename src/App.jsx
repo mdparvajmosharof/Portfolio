f6aaf11439e2fce8dff4941c06fc28d3ac590162
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2024-11-17 12:23:37 +0600

    scroll animation added

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import WhatIDo from "./Pages/WhatIDo"
 import Extracurricular from "./Pages/Extracurricular"
 import Rellax from "rellax"
 import { useEffect } from "react"
+import { motion, useScroll } from "motion/react"
 
 
 function App() {
@@ -33,28 +34,39 @@ function App() {
 
   }, [])
 
+  const { scrollYProgress } = useScroll();
+
   return (
     <>
+      <motion.div
+        className="progress-bar fixed top-0 right-0 left-0 h-1 bg-fuchsia-600 origin-left rounded-r-full z-10"
+        style={{ scaleX: scrollYProgress }}
+      />
+
+      <motion.div
+        className="progress-bar fixed bottom-0 right-0 left-0 h-1.5 z-10 bg-fuchsia-600 origin-right rounded-l-full"
+        style={{ scaleX: scrollYProgress }}
+      />
       <Navbar></Navbar>
 
       <div id="home">
         <Home></Home>
       </div>
 
-      
+
       <div className="flex justify-center bg-gradient-to-b  from-violet-700/20 py-14 px-6 border-violet-600 border-t-2 rounded-t-3xl mx-5 lg:mx-0">
-        <button  className="hvr-underline-from-center hvr-grow hvr-icon-down btn rounded-xl bg-violet-700/10 border-violet-600 hover:bg-violet-700/30 font-bold font-serif px-10">
+        <button className="hvr-underline-from-center hvr-grow hvr-icon-down btn rounded-xl bg-violet-700/10 border-violet-600 hover:bg-violet-700/30 font-bold font-serif px-10">
           <a href={resume} download={"mdparvajmosharof.pdf"}>Resume</a>
           <span className="text-xl hvr-icon"><IoDownloadOutline /></span>
 
         </button>
       </div>
 
-      
+
       <div className='absolute top-3 rellax right-36' data-rellax-speed="-5" data-rellax-zindex="5">
         <img className="w-36 opacity-30" src={graph1} alt="" />
       </div>
-      <div className='absolute top-3 rellax right-36' data-rellax-speed="-7" data-rellax-zindex="20">
+      <div className='absolute top-3 rellax right-36' data-rellax-speed="-5" data-rellax-zindex="20">
         <img className="opacity-25" src={graph3} alt="" />
       </div>
 
@@ -63,7 +75,7 @@ function App() {
         <AboutMe></AboutMe>
       </div>
 
-      
+
 
       <div className="pt-20 mx-5 lg:mx-0" id="projects">
         <Projects></Projects>
@@ -88,7 +100,7 @@ function App() {
       <div className="pt-20 mx-5 lg:mx-0">
         <Footer></Footer>
       </div>
-      
+
     </>
   )
 }
